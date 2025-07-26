a20a2f5adfb3860d3d58cd39c0b53e76e95c60b2
piyushSharma97 <piyushsharma70141@gmail.com>
2025-07-26 18:32:04 +0530

    change app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App() {
     <>
       <h1>Github Actions</h1>
       <h2>Complete course with 2 projects</h2>
+      <h3>Project 1: React App</h3>
+      <h3>Project 2: Node.js App</h3>
+      <h4>Course by: John Doe</h4> 
     </>
   )
 }
